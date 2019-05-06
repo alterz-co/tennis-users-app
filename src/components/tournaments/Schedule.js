6c6ba8758c6a373090cbbd8b96defa64619edde6
alterz01 <alterz.ml@gmail.com>
2019-05-06 14:15:51 +0800

    Add tournament updates, results, schedule and details

--- src/components/tournaments/Schedule.js
+++ src/components/tournaments/Schedule.js
@@ -1,23 +1,70 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View } from 'react-native';
+import {
+  Container,
+  Header,
+  Left,
+  Right,
+  Body,
+  Title,
+  Content,
+  Text,
+  Button,
+  Icon,
+  Spinner
+} from 'native-base';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import HTMLView from 'react-native-htmlview';
 
 class Schedule extends Component {
   render(){
+    const { schedule } = this.props;
+
+    if(!schedule){
+      return <Spinner color='grey'/>
+    }
+
     return(
-      <View style={styles.container}>
-        <Text>Schedule</Text>
-      </View>
+      <Container>
+        <Header>
+          <Left>
+            <Button iconLeft transparent dark onPress={() => this.props.navigation.goBack()}>
+              <Icon name='ios-arrow-back' />
+              <Text>Back</Text>
+            </Button>
+          </Left>
+          <Body>
+            <Title>Schedule</Title>
+          </Body>
+          <Right></Right>
+        </Header>
+        <Content padding>
+        {
+          schedule && schedule.map(schedule => {
+            return(
+              <View key={schedule.id} style={{ padding: 40 }}>
+                <HTMLView value={schedule.description}/>
+              </View>
+            )
+          })
+        }
+        </Content>
+      </Container>
     )
   }
 }
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
+const mapStateToProps = (state) => {
+  return {
+    schedule: state.firestore.ordered.schedule
+  }
+}
 
-export default Schedule;
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: 'schedule', where: [ 'tournamentId', '==', 'nNQpGmwFKqdqxS47Tehm' ] }
+  ])
+)(Schedule);
